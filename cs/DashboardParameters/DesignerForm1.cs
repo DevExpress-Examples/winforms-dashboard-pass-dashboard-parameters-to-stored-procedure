2bc9ed0937c6cb8cd3630e00fe68d1a5904b4fb1
Polina Tyureva <polina.tyureva@devexpress.com>
2023-02-02 12:55:43 +0400

    add cs solution

---
+++ cs/DashboardParameters/DesignerForm1.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace DashboardParameters {
+    public partial class DesignerForm1 : DevExpress.XtraBars.Ribbon.RibbonForm {
+        public DesignerForm1() {
+            InitializeComponent();
+            dashboardDesigner.CreateRibbon();
+            dashboardDesigner.LoadDashboard(@"Dashboards\dashboard1.xml");
+            dashboardDesigner.CustomParameters += DashboardDesigner_CustomParameters;
+        }
+
+        private void DashboardDesigner_CustomParameters(object sender, DevExpress.DashboardCommon.CustomParametersEventArgs e) {
+            var customerIDsparam = e.Parameters.FirstOrDefault(p => p.Name == "orderIDs"); ;
+            if (customerIDsparam != null) {
+                customerIDsparam.Value = new string[] {"10251", "10252", "10253", "10254"} ;
+
+            }
+        }
+    }
+}
